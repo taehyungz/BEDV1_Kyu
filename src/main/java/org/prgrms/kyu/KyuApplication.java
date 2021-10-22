c24545f4b7e704213d4f1b812632e235c1441347
Heejeong01110 <123rkdrkd@naver.com>
2021-10-22 17:57:06 +0900

    feat: 프로젝트 기본 셋팅

---
+++ src/main/java/org/prgrms/kyu/KyuApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.kyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KyuApplication.class, args);
+  }
+
+}
