37c43a20734ccd72f744ee3d03ef564a875f9ea5
eunseo2 <20180972@sungshin.ac.kr>
2021-10-24 00:13:50 +0900

    [feat: #3] JPA Auditing 활성화

--- src/main/java/org/prgrms/kyu/entity/BaseTimeEntity.java
+++ src/main/java/org/prgrms/kyu/entity/BaseTimeEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    @Column(name = "created_at", columnDefinition = "TIMESTAMP", updatable = false)
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP", updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
