e3280d69117dac3a0e36065db44ccb9f77e50e70
Heejeong01110 <123rkdrkd@naver.com>
2021-10-27 14:04:32 +0900

    [fix: #12]  controller 주소표기 수정

--- src/main/java/org/prgrms/kyu/controller/api/StoreRestController.java
+++ src/main/java/org/prgrms/kyu/controller/api/StoreRestController.java
@@ -1,4 +1,4 @@
-package org.prgrms.kyu.controller;
+package org.prgrms.kyu.controller.api;
 
 import java.util.List;
 import javassist.NotFoundException;
@@ -9,29 +9,34 @@ import org.prgrms.kyu.dto.StoreCreateRequest;
 import org.prgrms.kyu.dto.StoreFindResponse;
 import org.prgrms.kyu.service.StoreService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class StoreController {
+public class StoreRestController {
+
   private final StoreService storeService;
 
-  @GetMapping("/api/v1/stores")
+  @GetMapping("/stores")
   public ApiResponse<List<StoreFindResponse>> getAllStore(){
     return ApiResponse.ok(storeService.findAll());
   }
 
-  @GetMapping("/api/v1/stores/{id}")
+  @GetMapping("/stores/{id}")
   public ApiResponse<StoreFindResponse> getOneStore(@PathVariable Long id)
       throws NotFoundException {
     return ApiResponse.ok(storeService.findById(id));
   }
 
-  @PostMapping("/api/v1/stores")
-  public ApiResponse<Long> saveStore(@RequestParam StoreCreateRequest storeCreateRequest)
+  @PostMapping("/stores")
+  public ApiResponse<Long> saveStore(@ModelAttribute StoreCreateRequest storeCreateRequest)
       throws AuthenticationException {
     return ApiResponse.ok(storeService.save(storeCreateRequest));
   }
