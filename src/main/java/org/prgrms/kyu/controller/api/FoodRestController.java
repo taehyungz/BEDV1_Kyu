74f32ee64fccbe706607d39262a4f9798312261a
eunseo2 <20180972@sungshin.ac.kr>
2021-10-27 16:13:32 +0900

    [feat: #14]  공통 ApiResponse 적용

--- src/main/java/org/prgrms/kyu/controller/api/FoodRestController.java
+++ src/main/java/org/prgrms/kyu/controller/api/FoodRestController.java
@@ -1,6 +1,7 @@
 package org.prgrms.kyu.controller.api;
 
 import lombok.RequiredArgsConstructor;
+import org.prgrms.kyu.ApiResponse;
 import org.prgrms.kyu.dto.FoodRequest;
 import org.prgrms.kyu.service.FoodService;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ public class FoodRestController {
     private final FoodService foodService;
 
     @PostMapping("/stores/{storeId}/foods")
-    public Long create(@RequestBody FoodRequest request, @PathVariable Long storeId) {
-        return foodService.save(request, storeId);
+    public ApiResponse<Long> create(@RequestBody FoodRequest request, @PathVariable Long storeId) {
+        return ApiResponse.ok(foodService.save(request, storeId));
     }
 }
