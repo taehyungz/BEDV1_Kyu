5c657d91dd124323bed746db4e294efde6c88f56
eunseo2 <20180972@sungshin.ac.kr>
2021-10-28 15:03:07 +0900

    [feat: #14]  JpaAuditingConfiguration 추가

--- src/main/java/org/prgrms/kyu/KyuApplication.java
+++ src/main/java/org/prgrms/kyu/KyuApplication.java
@@ -2,9 +2,7 @@ package org.prgrms.kyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class KyuApplication {
 
