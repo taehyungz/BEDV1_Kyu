b8ccf459e7fecc644790386c0ff658f71501656a
thkthk97@naver.com <thkthk97@naver.com>
2021-10-30 02:03:39 +0900

    [fix: #13]  가게 저장 API 수정 - userId를 웹이 아니라 컨트롤러에서 받음

--- src/main/java/org/prgrms/kyu/controller/StoreController.java
+++ src/main/java/org/prgrms/kyu/controller/StoreController.java
@@ -1,23 +1,32 @@
 package org.prgrms.kyu.controller;
 
-import javax.naming.AuthenticationException;
 import lombok.RequiredArgsConstructor;
-import org.prgrms.kyu.dto.JoinRequest;
 import org.prgrms.kyu.dto.StoreCreateRequest;
+import org.prgrms.kyu.dto.UserInfo;
+import org.prgrms.kyu.service.SecurityService;
 import org.prgrms.kyu.service.StoreService;
+import org.prgrms.kyu.service.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.naming.AuthenticationException;
 
 @Controller
 @RequiredArgsConstructor
 public class StoreController {
   private final StoreService storeService;
+  private final SecurityService securityService;
+  private final UserService userService;
 
   @PostMapping("/store")
-  public String signUp(@ModelAttribute("storeCreateForm") StoreCreateRequest storeCreateRequest)
+  public String signUp(@ModelAttribute("storeCreateForm") StoreCreateRequest storeCreateRequest,
+                       Authentication authentication)
       throws AuthenticationException {
-    storeService.save(storeCreateRequest);
+    if (!securityService.isAuthenticated()) throw new AuthenticationException();
+    final UserInfo userInfo = userService.getUser(authentication.getName());
+    storeService.save(storeCreateRequest, userInfo.getId());
     return "redirect:/";
   }
 }
