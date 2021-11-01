f7c31b0de451ca1938aefae934ebd2b4bb98b392
Heejeong01110 <123rkdrkd@naver.com>
2021-11-01 12:58:28 +0900

    [fix: #12] @WebMvcTest로 변경

--- src/test/java/org/prgrms/kyu/service/StoreServiceTest.java
+++ src/test/java/org/prgrms/kyu/service/StoreServiceTest.java
@@ -1,82 +1,106 @@
 package org.prgrms.kyu.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javassist.NotFoundException;
 import javax.naming.AuthenticationException;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.prgrms.kyu.dto.JoinRequest;
 import org.prgrms.kyu.dto.StoreCreateRequest;
 import org.prgrms.kyu.dto.StoreFindResponse;
 import org.prgrms.kyu.entity.Store;
-import org.prgrms.kyu.entity.UserType;
+import org.prgrms.kyu.entity.User;
 import org.prgrms.kyu.repository.StoreRepository;
 import org.prgrms.kyu.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class StoreServiceTest {
 
-  @Autowired
+  @Spy
+  @InjectMocks
   StoreService storeService;
 
-  @Autowired
-  UserService userService;
-
-  @Autowired
+  @Mock
   StoreRepository storeRepository;
 
-  @Autowired
+
+  @Mock
+  UserService userService;
+
+  @Mock
   UserRepository userRepository;
 
-  Long userId;
-  Long storeId;
-  StoreCreateRequest storeCreateRequest;
+  Long fakeUserId;
+  long fakeStoreId;
+  User saveUser;
+  Store saveStore;
 
   @BeforeEach
   public void setUp() throws AuthenticationException {
-    userId = userService.join(
-        new JoinRequest("test1@test.com", "1234", "user", "nick", "Seoul", "STORE_OWNER"));
-    storeCreateRequest = new StoreCreateRequest(
+    //given
+    JoinRequest joinRequest = new JoinRequest(
+        "test1@test.com",
+        "1234",
+        "user",
+        "nick",
+        "Seoul",
+        "STORE_OWNER");
+
+    saveUser = new User(joinRequest);
+
+    fakeStoreId = 1L;
+    fakeUserId = 1L;
+    StoreCreateRequest storeCreateRequest = new StoreCreateRequest(
         "맘스터치",
         "01011112222",
         "맘스터치입니다.",
         "Seoul",
-        userId);
-    storeId = storeService.save(storeCreateRequest);
-  }
+        fakeUserId);
 
-  @AfterEach
-  public void cleanUp(){
-    storeService.deleteAll();
-    userRepository.deleteAll();
-  }
+    saveStore = storeCreateRequest.convertToStore(saveUser);
 
+
+    given(userRepository.save(any())).willReturn(saveUser);
+    given(storeRepository.save(any())).willReturn(saveStore);
+
+    //when
+    userRepository.save(saveUser);
+    storeService.save(storeCreateRequest);
+
+  }
 
 
   @Test
   @DisplayName("음식점을 생성할 수 있다.")
-  public void storeSaveTest() throws AuthenticationException {
-    //then
-    Optional<Store> findStore = storeRepository.findById(storeId);
-    Store store = storeCreateRequest.convertToStore(userService.findById(userId));
-    assertThat(findStore.get(),notNullValue());
-    StoreCreateRequest findStoreRequest = new StoreCreateRequest(
-        store.getName(),
-        store.getTelephone(),
-        store.getDescription(),
-        store.getLocation(),
-        store.getUser().getId());
-    assertThat(findStoreRequest,allOf(notNullValue(),samePropertyValuesAs(storeCreateRequest)));
+  public void storeSaveTest() {
+    //given
+    ReflectionTestUtils.setField(saveUser, "id", fakeUserId);
+    ReflectionTestUtils.setField(saveStore, "id", fakeStoreId);
+    given(storeRepository.findById(fakeStoreId)).willReturn(Optional.ofNullable(saveStore));
+
+    //when //then
+    Optional<Store> findStore = storeRepository.findById(fakeStoreId);
+
+    assertThat(findStore.get(),allOf(notNullValue(),samePropertyValuesAs(saveStore)));
   }
 
 
@@ -84,12 +108,19 @@ class StoreServiceTest {
   @Test
   @DisplayName("id로 음식점을 찾을 수 있다.")
   public void findByIdTest() throws NotFoundException {
+    //given
+    StoreFindResponse storeFindResponse = new StoreFindResponse(saveStore);
+    given(storeRepository.findById(fakeStoreId)).willReturn(Optional.ofNullable(saveStore));
+    doReturn(storeFindResponse).when(storeService).findById(fakeStoreId);
+
     //when
-    StoreFindResponse findStore = storeService.findById(storeId);
+    StoreFindResponse findStore = storeService.findById(fakeStoreId);
 
     //then
-    Optional<Store> store = storeRepository.findById(storeId);
-    assertThat(findStore,allOf(notNullValue(),samePropertyValuesAs(new StoreFindResponse(store.get()))));
+    Optional<Store> store = storeRepository.findById(fakeStoreId);
+    assertThat(findStore,allOf(
+        notNullValue(),
+        samePropertyValuesAs(new StoreFindResponse(store.get()))));
   }
 
 
@@ -97,21 +128,37 @@ class StoreServiceTest {
   @Test
   @DisplayName("모든 음식점을 찾을 수 있다.")
   public void findAllTest() throws AuthenticationException {
-    Long userId2 = userService.join(
-        new JoinRequest("test2@test.com", "1234", "user2", "nick2", "Seoul", "STORE_OWNER"));
-    StoreCreateRequest storeCreateRequest2 = new StoreCreateRequest(
-        "맘스터치2",
+    //given
+
+    Long fakeStoreId = 2L;
+    StoreCreateRequest storeCreateRequest = new StoreCreateRequest(
+        "맘스터치",
         "01011112222",
-        "맘스터치2입니다.",
+        "맘스터치입니다.",
         "Seoul",
-        userId2);
-    storeService.save(storeCreateRequest2);
+        fakeUserId);
+
+    Store saveStore2 = storeCreateRequest.convertToStore(saveUser);
+    given(storeRepository.save(any())).willReturn(saveStore2);
+    storeService.save(storeCreateRequest);
+
+
+    StoreFindResponse storeFindResponse = new StoreFindResponse(saveStore);
+    StoreFindResponse storeFindResponse2 = new StoreFindResponse(saveStore2);
+    doReturn(List.of(storeFindResponse, storeFindResponse2)).when(storeService).findAll();
+    given(storeRepository.findAll()).willReturn(List.of(saveStore, saveStore2));
 
     //when
     List<StoreFindResponse> all = storeService.findAll();
 
     //then
-    assertThat(all,hasSize(2));
+    List<StoreFindResponse> findAll = storeRepository.findAll().stream()
+        .map((StoreFindResponse::new))
+        .collect(Collectors.toList());
+
+    assertThat(all,allOf(
+        notNullValue(),
+        samePropertyValuesAs(findAll)));
   }
 
 }
